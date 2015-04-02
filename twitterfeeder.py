9584186ca98eda5340cf90b1031ef22b45fd6784
ben-en <ben@outernet.is>
2015-04-02 10:28:18 -0700

    Added ftp pushing

--- twitterfeeder.py
+++ twitterfeeder.py
@@ -1,6 +1,7 @@
 import os
 import json
 import argparse
+import ftplib
 from datetime import datetime
 from twitter import Twitter, OAuth
 
@@ -8,6 +9,29 @@ from twitter import Twitter, OAuth
 TOKEN = '2316526382-tP9Bbs1rMw1ReyqkgAiJdRAwNj58cAMXGT0VtrO'
 CON_KEY = 'XlQa2HVvxN4wU2KufRTvBlEy4'
 
+def push_to_ftp(json, name):
+    now = datetime.now()
+    ftp = ftplib.FTP()
+    with open(args.ip_list) as f:
+        for line in f:
+            u, p, i = line.split(':')
+            user = u.strip()
+            pass_key = p.strip()
+            ip = i.strip()
+            ftp.connect(ip)
+            print('%s | Connected | %s' % (now, ip))
+            ftp.login(user, pass_key)
+            print('%s | Logged in | %s' % (now, user))
+            try:
+                ftp.cwd('/files/tweets')
+            except ftplib.error_perm:
+                ftp.cwd('/files/')
+                ftp.mkd('tweets')
+                ftp.cwd('/files/tweets')
+            print('%s | Navigated | tweets' % now)
+            with open(json, 'rb') as file:
+                ftp.storbinary('STOR ' + name, file)
+            print('%s | Wrote | %s' % (now, name))
 
 
 if __name__ == "__main__":
@@ -21,6 +45,9 @@ if __name__ == "__main__":
     parser.add_argument('token_secret', metavar='KEY', help='Secret token, '
                         'looks something like this: XLaAx7XwBSlvOQPmFhAJnMP67r'
                         '4XcQIGmgHQPU84b6n3c')
+    parser.add_argument('--ip_list', metavar='FILE', help='List of ips in a '
+                        'user:pass:IP format, one item per line.',
+                        default='ips.lst')
     parser.add_argument('--out', metavar='DIR', help='Directory to output the '
                         'tweet file to.', default='out')
     args = parser.parse_args()
@@ -41,6 +68,9 @@ if __name__ == "__main__":
                            time, 'handle': handle},
                           sort_keys=True, separators=(',', ': '))
         json_output.append(dump)
-    with open(os.path.join(args.out, handle + '.json'), 'w') as f:
+    name = handle + '.json'
+    json_file = os.path.join(args.out, name)
+    with open(json_file, 'w') as f:
         output = '[{}]'.format(', '.join(json_output))
         f.write(output)
+    push_to_ftp(json_file, name)
