0c294b61d500c1ec6c0afa5053dfabcd9f00e96f
Ben Ennis <ben@outernet.is>
2015-05-27 15:32:05 -0700

    updated twitterfeeder for the getting of images

--- scripts/twitterfeeder.py
+++ scripts/twitterfeeder.py
@@ -1,7 +1,8 @@
 import os
 import json
-import argparse
 import ftplib
+import urllib
+import argparse
 from datetime import datetime
 from twitter import Twitter, OAuth
 
@@ -10,10 +11,10 @@ TOKEN = '2316526382-tP9Bbs1rMw1ReyqkgAiJdRAwNj58cAMXGT0VtrO'
 CON_KEY = 'XlQa2HVvxN4wU2KufRTvBlEy4'
 
 
-def push_to_ftp(json, name):
+def push_to_ftp(json, name, ip_list):
     now = datetime.now()
     ftp = ftplib.FTP()
-    with open(args.ip_list) as f:
+    with open(ip_list) as f:
         for line in f:
             u, p, i = line.split(':')
             user = u.strip()
@@ -35,42 +36,137 @@ def push_to_ftp(json, name):
             print('%s | Wrote | %s' % (now, name))
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Downloads feeds later than'
-                                     'specified post from specified feed')
+def get_image(tweet):
+    """ Checks if an image is linked in the tweet data and returns url """
+    try:
+        media = tweet['entities']['media']
+        if media[0]['type'] == "photo":
+            link = media[0]['media_url_https']
+            return link
+
+    except KeyError:
+        return False
+
+
+def get_tweets(out, handle, stoken, scon_key):
+    """ Retrieves tweets from Twitter API """
+    t = Twitter(auth=OAuth(TOKEN, stoken, CON_KEY, scon_key))
+
+    last_id = get_last_id(out, handle)
+    if last_id:
+        tweets = t.statuses.user_timeline(screen_name=handle, since_id=last_id)
+    else:
+        tweets = t.statuses.user_timeline(screen_name=handle)
+    return tweets
+
+
+def get_last_id(out, handle):
+    try:
+        with open(os.path.join(out, handle, 'last.txt'), 'r') as last_file:
+            last = last_file.readline()
+            print(last)
+            return last
+    except IOError:
+        return False
+
+
+def parse_tweet(tweet):
+    """ Removes unnecessary data from tweet and returns """
+    # Simple line to print tweets nicely
+    #print(json.dumps(tweet, sort_keys=False, indent=4, separators=(',', ': ')))
+
+    time_obj = datetime.strptime(tweet['created_at'],
+                                 '%a %b %d %H:%M:%S +0000 %Y')
+    date, time = time_obj.isoformat().split('T')
+    id = str(tweet['id'])
+    parsed = {'id': id, 'text': tweet['text'], 'date': date, 'time': time,
+            'handle': tweet['user']['screen_name']}
+    return parsed
+
+
+
+
+def write_tweet(out, tweet, ip_list):
+    """ Writes each tweet to a file named ID.json, with relevant image """
+    image = get_image(tweet)
+
+    # No longer a full tweet after next line
+    tweet = parse_tweet(tweet)
+    handle = tweet['handle']
+    dir = os.path.join(out, handle)
+    name = tweet['id'] + '.json'
+    json_file = os.path.join(dir, name)
+    if image:
+        ext = image[-4:]
+        tweet['img'] = ext
+        image_name = tweet['id'] + ext
+        image_out = os.path.join(dir, 'img', image_name)
+        try:
+            urllib.urlretrieve(image, image_out)
+        except IOError:
+            os.mkdir(os.path.join(dir, 'img'))
+            urllib.urlretrieve(image, image_out)
+    try:
+        with open(json_file, 'w') as f:
+            f.write(json.dumps(tweet, ensure_ascii=True, sort_keys=False, indent=4, separators=(',', ': ')))
+    except IOError:
+        try:
+            os.mkdir(dir)
+        except OSError:
+            os.mkdir(out)
+            os.mkdir(dir)
+        with open(json_file, 'w') as f:
+            f.write(json.dumps(tweet, sort_keys=False, indent=4, separators=(',', ': ')))
+    push_to_ftp(json_file, name, ip_list)
+
+
+
+def write_last(id, out, handle):
+    try:
+        with open(os.path.join(out, handle, 'last.txt'), 'w') as last_file:
+            last_file.write(id)
+    except IOError:
+        pass
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Downloads tweets later than an optionally specified tweet'
+        ' from a specified twitter handle')
     parser.add_argument('handle', metavar='USER', help='User name to get feed'
                         'for, looks like this: @BreakingNews.')
-    parser.add_argument('con_secret', metavar='KEY', help='Secret connection'
-                        'key, looks something like this: Cwtyb0ftzoxLrMGCKPdFD'
-                        '5gIAtgpMwUFCN5LAiaNkQzCbOKhRY')
-    parser.add_argument('token_secret', metavar='KEY', help='Secret token, '
-                        'looks something like this: XLaAx7XwBSlvOQPmFhAJnMP67r'
-                        '4XcQIGmgHQPU84b6n3c')
+    parser.add_argument('con_secret', metavar='CON-SECRET', help='Secret '
+                        'connection key, looks something like this: '
+                        'Cwtyb0ftzoxLrMGCKPdFD5gIAtgpMwUFCN5LAiaNkQzCbOKhRY')
+    parser.add_argument('token_secret', metavar='TOKEN-SECRET',
+                        help='Secret token, looks something like this: '
+                        'XLaAx7XwBSlvOQPmFhAJnMP67r 4XcQIGmgHQPU84b6n3c')
     parser.add_argument('--ip_list', metavar='FILE', help='List of ips in a '
                         'user:pass:IP format, one item per line.',
                         default='ips.lst')
     parser.add_argument('--out', metavar='DIR', help='Directory to output the '
-                        'tweet file to.', default='out')
+                        'tweet files to.', default='out')
+    parser.add_argument('--last-tweet', metavar='LAST-TWEET', help='Last tweet'
+                        'downloaded, for retrieving only new tweets')
     args = parser.parse_args()
 
-    t = Twitter(auth=OAuth(TOKEN, args.token_secret, CON_KEY, args.con_secret))
-
+    out = args.out
     handle = args.handle[1:]
-    json_output = []
+    stoken = args.token_secret
+    scon_key = args.con_secret
+    ip_list = args.ip_list
 
-    statuses = t.statuses.user_timeline(screen_name=handle)
-    for s in statuses:
-        time_obj = datetime.strptime(s['created_at'],
-                                     '%a %b %d %H:%M:%S +0000 %Y')
-        date, time = time_obj.isoformat().split('T')
-        id = str(s['id'])
-        dump = json.dumps({'id': id, 'text': s['text'], 'date': date, 'time':
-                           time, 'handle': handle},
-                          sort_keys=True, separators=(',', ': '))
-        json_output.append(dump)
-    name = handle + '.json'
-    json_file = os.path.join(args.out, name)
-    with open(json_file, 'w') as f:
-        output = '[{}]'.format(', '.join(json_output))
-        f.write(output)
-    push_to_ftp(json_file, name)
+    raw_tweets = get_tweets(out, handle, stoken, scon_key)
+
+    for raw_tweet in raw_tweets:
+        # Writes the first (most recent) id to last.txt in a given directory.
+        if raw_tweet == raw_tweets[0]:
+            write_last(str(raw_tweet['id']), out, handle)
+        write_tweet(out, raw_tweet, ip_list)
+
+    # Depricated
+    #push_to_ftp(json_file, name)
+
+
+if __name__ == "__main__":
+    main()
